b4a839137a46baaf2b6478bf9291e784e16949e3
Marcus Farias <marcusczupriniakifarias@gmail.com>
2025-08-16 19:15:39 -0300

    feat: add user entity and its value objects

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Domain.ValueObjects;
+
+namespace Domain.Entities;
+
+public class User : BaseEntity
+{
+
+    public string Cpf { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    //TODO: add role enum, email and address value objects
+    // public Enum Role { get; private set; }
+    // public Email Email { get; private set; } //EmailAddress from system.net.email
+    public string Password { get; private set; }
+    public bool IsActive { get; private set; }
+    public DateOnly BirthDate { get; private set; }
+    public Address Address { get; private set; }
+    
+    //TODO: property validation
+    public User(string cpf, string firstName, string lastName, string password, bool isActive, DateOnly birthDate,
+        Address address)
+    {
+        Cpf = cpf ?? throw new ArgumentNullException(nameof(cpf));
+        FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+        LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+        Password = password ?? throw new ArgumentNullException(nameof(password));
+        IsActive = isActive;
+        BirthDate = birthDate;
+        Address = address ?? throw new ArgumentNullException(nameof(address));
+    }
+    
+    public void Deactivate()
+    {
+        IsActive = false;
+    }
+}
---
+++ src/Domain/ValueObjects/ValueObject.cs
@@ -0,0 +1,34 @@
+﻿namespace Domain.ValueObjects;
+
+//https://learn.microsoft.com/en-us/dotnet/architecture/microservices/microservice-ddd-cqrs-patterns/implement-value-objects
+//https://www.milanjovanovic.tech/blog/value-objects-in-dotnet-ddd-fundamentals
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public virtual bool Equals(ValueObject? other) => other is not null && ValuesAreEqual(other);
+    
+    public override bool Equals(object? obj) => obj is ValueObject valueObject && ValuesAreEqual(valueObject);
+    
+    public override int GetHashCode() =>
+        GetAtomicValues().Aggregate(
+            0,
+            (hashcode, value) =>
+                HashCode.Combine(hashcode, value.GetHashCode()));
+    
+    protected abstract IEnumerable<object> GetAtomicValues();
+    
+    private bool ValuesAreEqual(ValueObject valueObject) =>
+        GetAtomicValues().SequenceEqual(valueObject.GetAtomicValues());
+
+    public static bool operator !=(ValueObject? a, ValueObject? b) => !(a == b);
+
+    public static bool operator ==(ValueObject? a, ValueObject? b)
+    {
+        if (a is null && b is null)
+            return true;
+
+        if (a is null || b is null)
+            return false;
+
+        return a.Equals(b);
+    }
+}
